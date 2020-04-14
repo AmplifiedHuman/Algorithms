5692b4d8c43c43bcccef0c99c0fb3b4860c604c5
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-04-14 11:39:26 +0800

    Added comments and refinements for Russian Peasant Algorithm

--- RussianPeasant.java
+++ RussianPeasant.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class RussianPeasant {
+    /* Russian Peasant Multiplication Algorithm
+     * Complexity: O(log n)
+     */
     public static int russianMultiply(int n, int m) {
+        // running sum
         int sum = 0;
         while (n != 0) {
+            // only add m to running sum if n is odd
             if (n % 2 != 0) {
                 sum += m;
             }
@@ -12,9 +19,14 @@ public class RussianPeasant {
     }
 
     public static void main(String[] args) {
-        final long startTime = System.nanoTime();
-        System.out.println(russianMultiply(Integer.MAX_VALUE / 2, Integer.MAX_VALUE / 2));
-        final long elapsedTime = System.nanoTime() - startTime;
-        System.out.println("Time taken: " + elapsedTime);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int x = Integer.parseInt(reader.nextLine());
+        System.out.print("Enter second number: ");
+        int y = Integer.parseInt(reader.nextLine());
+        long startTime = System.nanoTime();
+        System.out.println("Result: " + russianMultiply(x, y));
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Time taken: " + elapsedTime + "ns");
     }
 }
