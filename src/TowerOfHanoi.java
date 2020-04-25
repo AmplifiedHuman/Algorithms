134155a9d6c0de3eec6b03efc78cece5f52b3117
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-04-25 21:30:07 +0800

    added Tower Of Hanoi performance data and graph

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class TowerOfHanoi {
     // Runtime is O(2 ^ n)
     public static void solveHanoi(int n, String disk, String source, String destination, String auxiliary) {
@@ -11,6 +15,23 @@ public class TowerOfHanoi {
     }
 
     public static void main(String[] args) {
-        solveHanoi(3, "Disk", "A", "C", "B");
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the number of disks: ");
+        int n = Integer.parseInt(reader.nextLine());
+        solveHanoi(n, "Disk", "A", "C", "B");
+//        measureAndRecordPerformance(1, 23);
+    }
+
+    public static void measureAndRecordPerformance(int start, int end) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i < end; i++) {
+            long startTime = System.nanoTime();
+            solveHanoi(i, "Disk", "A", "C", "B");
+            long elapsedTime = System.nanoTime() - startTime;
+            sb.append(String.format("%d %d\n", i, elapsedTime));
+        }
+        FileWriter fileWriter = new FileWriter("src/output_files/tower_of_hanoi.txt");
+        fileWriter.write(sb.toString());
+        fileWriter.close();
     }
 }
